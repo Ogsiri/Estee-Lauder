ca1a021ec9ad9ae164e3548789215a9e238c6a3a
Sirin <salkhthmy501@gmail.com>
2022-06-16 17:03:38 -0400

    Probably the last

---
+++ Estee Lauder/Components/ProductCard.swift
@@ -0,0 +1,21 @@
+//
+//  ProductCard.swift
+//  Estee Lauder
+//
+//  Created by Hind Alhalafi on 6/15/22.
+//
+
+import SwiftUI
+
+struct ProductCard: View {
+    
+    
+    
+    }
+}
+
+struct ProductCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCard(product: productList[0])
+    }
+}
